a11c9cdfb01253727a1cc1a634c2457fc09c7368
AtelyPham <tinpham1412000@gmail.com>
2022-07-27 22:34:35 +0700

    feat: add `event` handler

---
+++ src/handlers/event.ts
@@ -0,0 +1,51 @@
+import { SubstrateEvent } from '@subql/types'
+import { Event } from '../types'
+import { ensureBlock } from './block'
+import { ensureExtrinsic } from './extrinsic'
+import { createDemocracy } from './democracy'
+
+export async function ensureEvent(event: SubstrateEvent) {
+  const block = await ensureBlock(event.block.block.header.number.toString())
+  const idx = event.idx
+  const recordId = `${block.id}-${idx}`
+  let data = await Event.get(recordId)
+  if (!data) {
+    data = new Event(recordId)
+    data.index = idx
+    data.blockId = block.id
+    data.blockNumber = BigInt(block.id)
+    data.timestamp = block.timestamp
+    await data.save()
+  }
+  return data
+}
+
+export async function createEvent(event: SubstrateEvent) {
+  const data = await ensureEvent(event)
+  data.section = event.event.section
+  data.method = event.event.method
+  // lazy method
+  data.docs = event.event.meta.docs.toString()
+  data.arguments = event.event.meta.args.toString()
+  data.data = event.event.data.toString()
+
+  const extrinsic = await (event.extrinsic
+    ? ensureExtrinsic(event.extrinsic)
+    : undefined)
+  if (extrinsic) {
+    data.extrinsicId = extrinsic.id
+  }
+
+  await data.save()
+
+  switch (data.section) {
+    case 'democracy': {
+      await createDemocracy(event)
+    }
+
+    default:
+      break
+  }
+
+  return data
+}
