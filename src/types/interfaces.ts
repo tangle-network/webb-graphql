498b3757d3ba065408310bef94350ea73afad9f0
ahmed korim <ahmedkorrim@gmail.com>
2022-08-29 20:27:38 +0200

    Add uncommited files

---
+++ src/types/interfaces.ts
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+
+// Auto-generated , DO NOT EDIT
+
+export interface ProposalTypeCount {
+
+    type: string;
+
+    count: string;
+
+    proposalId: string[];
+
+}
+
+
