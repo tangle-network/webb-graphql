36afc0dbb13c7c22d49b09ff740aed00c5f29e89
ahmed korim <ahmedkorrim@gmail.com>
2022-11-16 14:41:20 +0200

    Cleanup

--- src/handlers/source.ts
+++ src/handlers/source.ts
@@ -20,44 +20,22 @@ export async function addHb(
   sourceId: string
 ): Promise<[SourceState, number]> {
   const source = await ensureSource(sourceId)
-  let numberOfHeartbeats = 0
-  let updated = false
-  if (source.heartBeatCounters.length === 0) {
+  let numberOfHeartbeats
+  const hb = source.heartBeatCounters.find((hb) => hb.authorityId === accountId)
+  // set if not exist
+  if (!hb) {
     numberOfHeartbeats = 1
-    logger.info(
-      `Set the heartbeat for ${accountId} to be ${numberOfHeartbeats}`
-    )
     source.heartBeatCounters.push({
       authorityId: accountId,
       numberOfHeartBeats: 1,
     })
   } else {
-    for (const [
-      index,
-      heartBeastCounter,
-    ] of source.heartBeatCounters.entries()) {
-      if (heartBeastCounter.authorityId === accountId) {
-        numberOfHeartbeats = heartBeastCounter.numberOfHeartBeats + 1
-        heartBeastCounter.numberOfHeartBeats =
-          heartBeastCounter.numberOfHeartBeats + 1
-        logger.info(
-          `Updated the heartbeat for ${accountId} to be ${numberOfHeartbeats}`
-        )
-        updated = true
-      }
-      // the entry wasn't there, it's added here
-      if (index + 1 === source.heartBeatCounters.length && !updated) {
-        numberOfHeartbeats = 1
-        logger.info(
-          `Set the heartbeat for ${accountId} to be ${numberOfHeartbeats}`
-        )
-        source.heartBeatCounters.push({
-          authorityId: accountId,
-          numberOfHeartBeats: 1,
-        })
-      }
-    }
+    // update if  exist
+    numberOfHeartbeats = hb.numberOfHeartBeats + 1
+
+    hb.numberOfHeartBeats = hb.numberOfHeartBeats + 1
   }
+
   await source.save()
   return [source, numberOfHeartbeats]
 }
