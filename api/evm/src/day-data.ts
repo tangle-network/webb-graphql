6bc3725011dc8b71ec7add38b5d5bbf09f757be8
ahmed <ahmedkorrim@gmail.com>
2023-05-17 15:54:47 +0300

    Enhance day data with more metadata

--- api/evm/src/day-data.ts
+++ api/evm/src/day-data.ts
@@ -1,11 +1,11 @@
-import { BigInt, ethereum } from '@graphprotocol/graph-ts';
-import { VAnchor, VAnchorDayData } from '../generated/schema';
-import { ExternalData, TransactionType } from './utils/transact';
+import { BigInt, ethereum } from "@graphprotocol/graph-ts";
+import { VAnchor, VAnchorDayData } from "../generated/schema";
+import { TransactionType } from "./utils/transact";
 
 export function ensureDay(block: ethereum.Block, vAnchor: VAnchor): VAnchorDayData {
   const timestamp = block.timestamp.toI32();
   const dayIndex = timestamp / 86400; // rounded
-  const dayId = dayIndex.toString().concat('-').concat(vAnchor.id);
+  const dayId = dayIndex.toString().concat("-").concat(vAnchor.id);
   const dayData = VAnchorDayData.load(dayId);
 
   if (dayData) {
@@ -16,14 +16,54 @@ export function ensureDay(block: ethereum.Block, vAnchor: VAnchor): VAnchorDayDa
 
   newDayData.date = timestamp;
   newDayData.vAnchor = vAnchor.id;
-  newDayData.fees = BigInt.fromI32(0);
-  newDayData.volume = BigInt.fromI32(0);
-  newDayData.numberOfDeposits = BigInt.fromI32(0);
+  newDayData.wrappingFees = BigInt.zero();
+  newDayData.unWrappingFees = BigInt.zero();
+  newDayData.relayerFees = BigInt.zero();
+  newDayData.fees = BigInt.zero();
+
+  newDayData.volume = BigInt.zero();
+
+  newDayData.numberOfDeposits = BigInt.zero();
+  newDayData.numberOfWithdraws = BigInt.zero();
+  newDayData.numberOfTransfers = BigInt.zero();
+
   newDayData.depositTx = [];
+  newDayData.withdrawTx = [];
+  newDayData.transferTx = [];
   newDayData.startBlockNumber = block.number;
   newDayData.save();
 
   return newDayData;
+}
+
+export class DayDataPayload {
+  public relayerFees: BigInt;
+  public wrappingFees: BigInt;
+  public unWrappingFees: BigInt;
+  public finalAmount: BigInt;
+  public txType: TransactionType;
+
+  constructor(
+    relayerFees: BigInt,
+    wrappingFees: BigInt,
+    unWrappingFees: BigInt,
+    finalAmount: BigInt,
+    txType: TransactionType
+  ) {
+    this.relayerFees = relayerFees;
+    this.wrappingFees = wrappingFees;
+    this.unWrappingFees = unWrappingFees;
+    this.finalAmount = finalAmount;
+    this.txType = txType;
+  }
+
+  static default(): DayDataPayload {
+    return new DayDataPayload(BigInt.zero(), BigInt.zero(), BigInt.zero(), BigInt.zero(), TransactionType.Deposit);
+  }
+
+  get totalFees(): BigInt {
+    return this.relayerFees.plus(this.wrappingFees).plus(this.unWrappingFees);
+  }
 }
 
 /**
@@ -33,25 +73,38 @@ export function ensureDay(block: ethereum.Block, vAnchor: VAnchor): VAnchorDayDa
 export function updateVAnchorDayData(
   block: ethereum.Block,
   vAnchor: VAnchor,
-  extData: ExternalData,
+  dayDataPayload: DayDataPayload,
   txId: string
 ): void {
   const vAnchorDayData = ensureDay(block, vAnchor);
-  const finalAmount = extData.getFinalAmount();
-  const fee = extData.getFee();
-  const txType = extData.getTransactionType();
+  const finalAmount = dayDataPayload.finalAmount;
+  const fee = dayDataPayload.totalFees;
+  const txType = dayDataPayload.txType;
   // Check for deposit tx to store the number of deposits and other relatd data
   if (txType === TransactionType.Deposit) {
     vAnchorDayData.numberOfDeposits = vAnchorDayData.numberOfDeposits.plus(BigInt.fromI32(1));
     const depositTransactions = vAnchorDayData.depositTx;
-    depositTransactions.push(txId)
-
+    depositTransactions.push(txId);
     vAnchorDayData.depositTx = depositTransactions;
     vAnchorDayData.volume.plus(finalAmount);
   } else if (txType === TransactionType.Withdraw) {
     vAnchorDayData.volume.minus(finalAmount);
+    vAnchorDayData.numberOfWithdraws = vAnchorDayData.numberOfWithdraws.plus(BigInt.fromI32(1));
+
+    const withdrawTransactions = vAnchorDayData.depositTx;
+    withdrawTransactions.push(txId);
+    vAnchorDayData.withdrawTx = withdrawTransactions;
+  } else if (txType === TransactionType.Transfer) {
+    const TransferTransactions = vAnchorDayData.depositTx;
+    TransferTransactions.push(txId);
+    vAnchorDayData.transferTx = TransferTransactions;
+    vAnchorDayData.numberOfTransfers = vAnchorDayData.numberOfTransfers.plus(BigInt.fromI32(1));
   }
   // Store fees regardless of the transactin type
   vAnchorDayData.fees = vAnchorDayData.fees.plus(fee);
+  vAnchorDayData.relayerFees = vAnchorDayData.relayerFees.plus(dayDataPayload.relayerFees);
+  vAnchorDayData.wrappingFees = vAnchorDayData.wrappingFees.plus(dayDataPayload.wrappingFees);
+  vAnchorDayData.unWrappingFees = vAnchorDayData.unWrappingFees.plus(dayDataPayload.wrappingFees);
+
   vAnchorDayData.save();
 }
