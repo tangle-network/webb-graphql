a3a836f8fe9489a35ef791fe8a1dac48cbb2a736
Elkrem Root <137823962+elkremroot@users.noreply.github.com>
2023-07-05 12:06:02 +0100

    Init new graph (#102)
    * init new graph
    
    * make scripts better
    
    * add scheielded script
    
    ---------
    
    Co-authored-by: Nawaz Dhandala <hello@nawazdhandala.com>

---
+++ packages/vanchor/src/token/token.ts
@@ -0,0 +1,36 @@
+import { Address } from '@graphprotocol/graph-ts';
+import { erc20 } from '../../generated/VAnchor/erc20';
+import { Token } from '../../generated/schema';
+
+export function ensureToken(tokenAddress: Address): Address {
+    const maybeToken = Token.load(tokenAddress);
+    if (maybeToken) {
+        return tokenAddress;
+    }
+    const token = new Token(tokenAddress);
+
+    const tokenContract = erc20.bind(tokenAddress);
+    const name = tokenContract.try_name();
+    if (name.reverted) {
+        token.name = 'Ether';
+    } else {
+        token.name = name.value;
+    }
+    const symbol = tokenContract.try_symbol();
+    if (symbol.reverted) {
+        token.symbol = 'ETH';
+    } else {
+        token.symbol = symbol.value;
+    }
+    const decimals = tokenContract.try_decimals();
+    if (decimals.reverted) {
+        token.decimals = 18;
+    } else {
+        token.decimals = decimals.value;
+    }
+
+    token.address = tokenAddress;
+
+    token.save();
+    return tokenAddress;
+}
---
+++ packages/vanchor/tests/vanchor.test.ts
@@ -0,0 +1,64 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { BigInt, Bytes, Address } from "@graphprotocol/graph-ts"
+import { EdgeAddition } from "../generated/schema"
+import { EdgeAddition as EdgeAdditionEvent } from "../generated/vanchor/vanchor"
+import { handleEdgeAddition } from "../src/vanchor"
+import { createEdgeAdditionEvent } from "./vanchor-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let chainID = BigInt.fromI32(234)
+    let latestLeafIndex = BigInt.fromI32(234)
+    let merkleRoot = BigInt.fromI32(234)
+    let newEdgeAdditionEvent = createEdgeAdditionEvent(
+      chainID,
+      latestLeafIndex,
+      merkleRoot
+    )
+    handleEdgeAddition(newEdgeAdditionEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("EdgeAddition created and stored", () => {
+    assert.entityCount("EdgeAddition", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "EdgeAddition",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "chainID",
+      "234"
+    )
+    assert.fieldEquals(
+      "EdgeAddition",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "latestLeafIndex",
+      "234"
+    )
+    assert.fieldEquals(
+      "EdgeAddition",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "merkleRoot",
+      "234"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
