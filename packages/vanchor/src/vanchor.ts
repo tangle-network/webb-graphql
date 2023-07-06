287cd8ec5d27ee045930cc5c386e1437a9b0870b
Drew Stone <drewstone329@gmail.com>
2023-07-06 12:35:56 -0600

    Updates

--- packages/vanchor/src/vanchor.ts
+++ packages/vanchor/src/vanchor.ts
@@ -4,106 +4,106 @@ import {
   Insertion as InsertionEvent,
   NewCommitment as NewCommitmentEvent,
   NewNullifier as NewNullifierEvent,
-  PublicKey as PublicKeyEvent
-} from "../generated/vanchor/vanchor"
+  PublicKey as PublicKeyEvent,
+} from '../generated/vanchor/vanchor';
 import {
   EdgeAddition,
   EdgeUpdate,
   Insertion,
   NewCommitment,
   NewNullifier,
-  PublicKey
-} from "../generated/schema"
+  PublicKey,
+} from '../generated/schema';
 
 import { handleTransaction } from './transaction/schieldedTransaction';
 
 export function handleEdgeAddition(event: EdgeAdditionEvent): void {
-  let entity = new EdgeAddition(
+  const entity = new EdgeAddition(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.chainID = event.params.chainID
-  entity.latestLeafIndex = event.params.latestLeafIndex
-  entity.merkleRoot = event.params.merkleRoot
+  );
+  entity.chainID = event.params.chainID;
+  entity.latestLeafIndex = event.params.latestLeafIndex;
+  entity.merkleRoot = event.params.merkleRoot;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleEdgeUpdate(event: EdgeUpdateEvent): void {
-  let entity = new EdgeUpdate(
+  const entity = new EdgeUpdate(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.chainID = event.params.chainID
-  entity.latestLeafIndex = event.params.latestLeafIndex
-  entity.merkleRoot = event.params.merkleRoot
+  );
+  entity.chainID = event.params.chainID;
+  entity.latestLeafIndex = event.params.latestLeafIndex;
+  entity.merkleRoot = event.params.merkleRoot;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleInsertion(event: InsertionEvent): void {
-  let entity = new Insertion(
+  const entity = new Insertion(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.commitment = event.params.commitment
-  entity.leafIndex = event.params.leafIndex
-  entity.timestamp = event.params.timestamp
-  entity.newMerkleRoot = event.params.newMerkleRoot
+  );
+  entity.commitment = event.params.commitment;
+  entity.leafIndex = event.params.leafIndex;
+  entity.timestamp = event.params.timestamp;
+  entity.newMerkleRoot = event.params.newMerkleRoot;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 
-  handleTransaction(event)
+  handleTransaction(event);
 }
 
 export function handleNewCommitment(event: NewCommitmentEvent): void {
-  let entity = new NewCommitment(
+  const entity = new NewCommitment(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.commitment = event.params.commitment
-  entity.subTreeIndex = event.params.subTreeIndex
-  entity.leafIndex = event.params.leafIndex
-  entity.encryptedOutput = event.params.encryptedOutput
+  );
+  entity.commitment = event.params.commitment;
+  entity.subTreeIndex = event.params.subTreeIndex;
+  entity.leafIndex = event.params.leafIndex;
+  entity.encryptedOutput = event.params.encryptedOutput;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleNewNullifier(event: NewNullifierEvent): void {
-  let entity = new NewNullifier(
+  const entity = new NewNullifier(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.nullifier = event.params.nullifier
+  );
+  entity.nullifier = event.params.nullifier;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handlePublicKey(event: PublicKeyEvent): void {
-  let entity = new PublicKey(
+  const entity = new PublicKey(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.key = event.params.key
+  );
+  entity.owner = event.params.owner;
+  entity.key = event.params.key;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
