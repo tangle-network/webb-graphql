20284c0301406e41afc76628bccddef1ff0d6fcd
Nawaz Dhandala <hello@nawazdhandala.com>
2023-07-10 13:40:32 +0100

    fix parsing of date

--- packages/vanchor-client/src/queries/TotalValueLockedHistory.ts
+++ packages/vanchor-client/src/queries/TotalValueLockedHistory.ts
@@ -5,7 +5,7 @@ export interface TotalValueLockedByChainHistoryItem { chainName: string, totalVa
 
 export const GetVAnchorTotalValueLockedByChain = async (chainName: string, vAnchorAddress: string, startTimestamp: Date, endTimestamp: Date): Promise<TotalValueLockedByChainHistoryItem> => {
 
-    const query = `
+  const query = `
   query TotalValueLocked {
   vanchorTotalValueLockedEvery15Mins(
     where: {endInterval_lte: "${DateUtil.fromDateToEpoch(endTimestamp)}", startInterval_gte: "${DateUtil.fromDateToEpoch(startTimestamp)}", vAnchorAddress: "${vAnchorAddress.toLowerCase()}"}
@@ -17,17 +17,17 @@ export const GetVAnchorTotalValueLockedByChain = async (chainName: string, vAnch
   }
 }
 `
-    const result = await execute(query, {}, {
-        chainName,
-    })
+  const result = await execute(query, {}, {
+    chainName,
+  })
 
-    return result.data.vanchorTotalValueLockedEvery15Mins?.map((item: any) => {
-        return {
-            totalValueLocked: item?.totalValueLocked,
-            chainName: chainName,
-            startInterval: new Date(item?.startInterval),
-            endInterval: new Date(item?.endInterval),
-            vAnchorAddress: item?.vAnchorAddress
-        }
-    });
+  return result.data.vanchorTotalValueLockedEvery15Mins?.map((item: any) => {
+    return {
+      totalValueLocked: item?.totalValueLocked,
+      chainName: chainName,
+      startInterval: DateUtil.fromEpochToDate(parseInt(item?.startInterval)),
+      endInterval: DateUtil.fromEpochToDate(parseInt(item?.endInterval)),
+      vAnchorAddress: item?.vAnchorAddress
+    }
+  });
 }
