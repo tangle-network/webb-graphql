b116e6eb66e977f4aafa6e5248a56a607cebd0ca
Nawaz Dhandala <hello@nawazdhandala.com>
2023-07-11 13:56:25 +0100

    handle wrapping and unwrapping events

--- packages/vanchor/src/fungible-token-wrapper.ts
+++ packages/vanchor/src/fungible-token-wrapper.ts
@@ -1,8 +1,27 @@
 import {
     Wrapping as WrappingEvent,
+    Unwrapping as UnwrappingEvent,
 } from "../generated/vanchor/FungibleTokenWrapper";
-import { log } from "@graphprotocol/graph-ts";
+import { Bytes, ethereum, log } from "@graphprotocol/graph-ts";
+import { recordTotalFees } from "./wrappingFees";
+import { recordFeeFor15MinsInterval } from "./wrappingFees/15MinsInterval";
+import { getTxnInputDataToDecode } from "./utils/token";
 
 export function handleWrapping(event: WrappingEvent): void {
-    log.info("Wrapping event", []);
+
+    if (event.params !== null) {
+
+        const vAnchorAddress = event.params.recipient;
+        const tokenAddress = event.params.tokenAddress;
+        const feeAmount = event.params.wrappingFee;
+        // TODO - MAke sure this is a vAnchor address, otherwise skip.
+        // Record Wrapping Fees
+        recordTotalFees(vAnchorAddress, tokenAddress, feeAmount);
+        recordFeeFor15MinsInterval(vAnchorAddress, tokenAddress, feeAmount, event.block.timestamp);
+    }
+}
+
+
+export function handleUnwrapping(event: UnwrappingEvent): void {
+    log.info("Unwrapping event", []);
 }
