e67a117030c857f9671ed090cf1471b5ccbcddb6
Elkrem Root <137823962+elkremroot@users.noreply.github.com>
2023-07-11 14:33:10 +0100

    Wrapping and Unwrapping events  (#109)
    * update abi
    
    * handle wrapping and unwrapping events
    
    ---------
    
    Co-authored-by: Nawaz Dhandala <hello@nawazdhandala.com>

--- packages/vanchor/src/transaction/shieldedTransaction.ts
+++ packages/vanchor/src/transaction/shieldedTransaction.ts
@@ -12,15 +12,9 @@ import { recordTotalValueLocked } from '../totalValueLocked';
 import { recordTotalFees } from '../relayerFees';
 import { recordFeeFor15MinsInterval } from '../relayerFees/15MinsInterval';
 import { record15MinsIntervalTotalValueLocked } from '../totalValueLocked/15MinsInterval';
-import { isNativeToken } from '../utils/token';
+import { getTxnInputDataToDecode, isNativeToken } from '../utils/token';
 
-export function getTxnInputDataToDecode(txInput: Bytes): Bytes {
-    const inputDataHexString = txInput.toHexString().slice(10); //take away function signature: '0x????????'
-    const hexStringToDecode =
-        '0x0000000000000000000000000000000000000000000000000000000000000020' +
-        inputDataHexString; // prepend tuple offset
-    return Bytes.fromByteArray(Bytes.fromHexString(hexStringToDecode));
-}
+
 
 export const handleTransaction = (event: Insertion): void => {
     // Check if the transaction is already handled
