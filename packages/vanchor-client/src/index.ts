a2c8e3e152a3069e2eca7ec225466dfc5a0e6036
Nawaz Dhandala <hello@nawazdhandala.com>
2023-07-20 15:01:46 +0100

    export SubgraphUrl

--- packages/vanchor-client/src/index.ts
+++ packages/vanchor-client/src/index.ts
@@ -9,8 +9,10 @@ import { GetVAnchorTotalRelayerFeeByChain, GetVAnchorTotalRelayerFeeByChainAndBy
 import { GetVAnchorRelayerFeeByChainHistory, GetVAnchorRelayerFeeByChainAndByTokenHistory, GetVAnchorsRelayerFeeByChainHistory, GetVAnchorsRelayerFeeByChainsHistory } from './queries/RelayerFeeHistory'
 import { GetVAnchorTotalWrappingFeeByChain, GetVAnchorTotalWrappingFeeByChainAndByToken, GetVAnchorsTotalWrappingFeeByChain, GetVAnchorsTotalWrappingFeeByChains } from './queries/WrappingFee'
 import { GetVAnchorWrappingFeeByChainHistory, GetVAnchorWrappingFeeByChainAndByTokenHistory, GetVAnchorsWrappingFeeByChainHistory, GetVAnchorsWrappingFeeByChainsHistory } from './queries/WrappingFeeHistory'
+import { SubgraphUrl } from '../config'
 
 const result = {
+    SubgraphUrl,
     RelayerFee: {
         GetVAnchorTotalRelayerFeeByChain, GetVAnchorTotalRelayerFeeByChainAndByToken, GetVAnchorsTotalRelayerFeeByChain, GetVAnchorsTotalRelayerFeeByChains
     },
