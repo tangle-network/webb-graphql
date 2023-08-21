c0bbbca7808d511f82f14e1f27bcf1fd1b9fa3ca
Shady Khalifa <dev+github@shadykhalifa.me>
2023-08-21 14:18:17 +0300

    Add Nix and Flakes for devenv

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Webb graphql development environment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "grpahql";
+          nativeBuildInputs = [ ];
+          buildInputs = [
+            pkgs.nodePackages.typescript-language-server
+            pkgs.nodejs_18
+            pkgs.nodePackages.yarn
+          ];
+          packages = [ ];
+        };
+      });
+}
