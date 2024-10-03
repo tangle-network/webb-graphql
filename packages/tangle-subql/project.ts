f25165f3075fb1c40d2b725edbe3396cb9787d85
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2024-10-03 18:35:50 +0700

    feat: setup tangle-subql project

---
+++ packages/tangle-subql/project.ts
@@ -0,0 +1,85 @@
+import {
+  SubstrateDatasourceKind,
+  SubstrateHandlerKind,
+  SubstrateProject,
+} from "@subql/types";
+
+import * as dotenv from 'dotenv';
+import path from 'path';
+
+const mode = process.env.NODE_ENV || 'production';
+
+// Load the appropriate .env file
+const dotenvPath = path.resolve(__dirname, `.env${mode !== 'production' ? `.${mode}` : ''}`);
+dotenv.config({ path: dotenvPath });
+
+// Can expand the Datasource processor types via the genreic param
+const project: SubstrateProject = {
+  specVersion: "1.0.0",
+  version: "0.0.1",
+  name: "polkadot-starter",
+  description:
+    "This project can be used as a starting point for developing your SubQuery project",
+  runner: {
+    node: {
+      name: "@subql/node",
+      version: ">=3.0.1",
+    },
+    query: {
+      name: "@subql/query",
+      version: "*",
+    },
+  },
+  schema: {
+    file: "./schema.graphql",
+  },
+  network: {
+    /* The genesis hash of the network (hash of block 0) */
+    chainId: process.env.CHAIN_ID!,
+    /**
+     * These endpoint(s) should be public non-pruned archive node
+     * We recommend providing more than one endpoint for improved reliability, performance, and uptime
+     * Public nodes may be rate limited, which can affect indexing speed
+     * When developing your project we suggest getting a private API key
+     * If you use a rate limited endpoint, adjust the --batch-size and --workers parameters
+     * These settings can be found in your docker-compose.yaml, they will slow indexing but prevent your project being rate limited
+     */
+    endpoint: process.env.ENDPOINT!?.split(',') as string[] | string,
+  },
+  dataSources: [
+    {
+      kind: SubstrateDatasourceKind.Runtime,
+      startBlock: 1,
+      mapping: {
+        file: "./dist/index.js",
+        handlers: [
+          /*{
+            kind: SubstrateHandlerKind.Block,
+            handler: "handleBlock",
+            filter: {
+              modulo: 100,
+            },
+          },*/
+          /*{
+            kind: SubstrateHandlerKind.Call,
+            handler: "handleCall",
+            filter: {
+              module: "balances",
+            },
+          },*/
+          {
+            kind: SubstrateHandlerKind.Event,
+            handler: "handleEvent",
+            filter: {
+              module: "balances",
+              method: "Transfer",
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
+
+// Must set default to the project instance
+export default project;
